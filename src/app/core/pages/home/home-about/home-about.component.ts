1c0e0cd3610ceeeba448c18ced6a46930b2f9370
KamilFilar <49237105+KamilFilar@users.noreply.github.com>
2022-05-16 16:45:39 +0200

    Add home-about component

---
+++ src/app/core/pages/home/home-about/home-about.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-about',
+  templateUrl: './home-about.component.html',
+  styleUrls: ['./home-about.component.scss']
+})
+
+export class HomeAboutComponent implements OnInit {
+
+  imgPath = './../../../../../assets/images/author-photo.jpg';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
