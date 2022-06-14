9e96539b66e6b4ea683e0832a3943cb4d5bca3ea
KamilFilar <49237105+KamilFilar@users.noreply.github.com>
2022-06-14 10:23:50 +0200

    Move components from core dir to home module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,27 +12,9 @@ import { ShopModule } from './modules/shop/shop.module';
 // Components
 import { AppComponent } from './app.component';
 
-
-import { HomeComponent } from './core/pages/home/home.component';
-import { HomeWelcomeComponent } from './core/pages/home/home-welcome/home-welcome.component';
-import { HomeAboutComponent } from './core/pages/home/home-about/home-about.component';
-import { HomeBlogComponent } from './core/pages/home/home-blog/home-blog.component';
-import { HomeYoutubeComponent } from './core/pages/home/home-youtube/home-youtube.component';
-import { HomeShopComponent } from './core/pages/home/home-shop/home-shop.component';
-import { CaruselItemComponent } from './core/pages/home/home-shop/carusel-item/carusel-item.component';
-import { ShopComponent } from './modules/shop/shop.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    HomeWelcomeComponent,
-    HomeAboutComponent,
-    HomeBlogComponent,
-    HomeYoutubeComponent,
-    HomeShopComponent,
-    CaruselItemComponent,
-    ShopComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -6,10 +6,25 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { MaterialModule } from '../angular-material/material.module';
 // Components
 import { HomeComponent } from './home.component';
+import { HomeWelcomeComponent } from './components/home-welcome/home-welcome.component';
+import { HomeAboutComponent } from './components/home-about/home-about.component';
+import { HomeBlogComponent } from './components/home-blog/home-blog.component';
+import { HomeShopComponent } from './components/home-shop/home-shop.component';
+import { HomeYoutubeComponent } from './components/home-youtube/home-youtube.component';
+// Subcomponents
+import { CaruselItemComponent } from './components/home-shop/carusel-item/carusel-item.component';
 
 @NgModule({
   declarations: [
+    // Components
     HomeComponent,
+    HomeWelcomeComponent,
+    HomeAboutComponent,
+    HomeBlogComponent,
+    HomeShopComponent,
+    HomeYoutubeComponent,
+    // Subcomponents
+    CaruselItemComponent
   ],
   imports: [
     BrowserModule,
@@ -17,6 +32,9 @@ import { HomeComponent } from './home.component';
     SharedModule,
     MaterialModule,
     FontAwesomeModule
+  ],
+  exports: [
+    HomeComponent
   ],
   providers: [],
   bootstrap: [HomeModule]
